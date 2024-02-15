98fbecb25e0d3431a7e4e324b1ad2e3268322044
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-02-15 18:42:50 +0000

    ジャンル機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+namespace :admin do
+  resources :genres,only: [:edit, :create, :index, :update]
+end
+
 end
