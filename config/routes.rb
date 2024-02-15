997735ad4f6b41fae79c7bb5a4e6a0a09a36897b
ohira0213 <153938130+ohira0213@users.noreply.github.com>
2024-02-15 11:50:49 +0000

    ohira

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :publics
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  root to: "homes#top"
+  get 'home/about' => 'homes#about', as: 'about'
 end
