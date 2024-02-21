bd0fa9aa5e92d78624e52a30067fbbb2af6016c6
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-02-21 12:31:02 +0000

    注文詳細ページ:　注文内容以外エラー解消

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :public
   has_many :cart_items
+  has_many :order_details
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { wait_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, finish_prepare: 4 }
 # 　enum order_status: [ :wait_payment, :confirm_payment, :making, :preparing_ship, :finish_prepare ]
