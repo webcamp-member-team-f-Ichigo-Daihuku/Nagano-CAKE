c62260efb15c983a1634652f6570c59367e21f4c
Kohei-70 <152633774+Kohei-70@users.noreply.github.com>
2024-02-22 11:53:00 +0000

    製作ステータス追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,4 @@
 class Admin::OrdersController < ApplicationController
-  # def index
-  #   @orders = Order.all
-  # end
-
   def show
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: @order.id)
