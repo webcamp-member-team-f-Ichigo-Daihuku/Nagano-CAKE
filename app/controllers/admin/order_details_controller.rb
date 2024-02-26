a9ad9e3459a08209779816cdd6b27813ab5b344b
Kohei-70 <152633774+Kohei-70@users.noreply.github.com>
2024-02-26 08:08:12 +0000

    微修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,5 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    
     @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
       flash[:notice] = "更新しました♪"
